2d8cb3cfa990a2f39c924f8976fe466b3c985f18
kotyara12 <kotyara12@users.noreply.github.com>
2021-11-14 21:37:41 +0300

    Optimize

--- def_consts.h
+++ def_consts.h
@@ -16,7 +16,6 @@
 #include "def_scheduler.h"
 #include "def_openmon.h"
 #include "def_thingspeak.h"
-#include "def_tg_notify.h"
 #include "def_tg_messages.h"
 #include "def_tg_task.h"
 #include "def_tg_ping.h"
