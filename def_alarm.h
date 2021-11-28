da42de527c856833395b4fdf92cb4b3b7b3edb10
kotyara12 <kotyara12@users.noreply.github.com>
2021-11-28 11:17:56 +0300

    Change default alarm settings

--- def_alarm.h
+++ def_alarm.h
@@ -6,13 +6,19 @@
 #define CONFIG_ALARM_STATIC_ALLOCATION 1
 // EN: Stack size for the fire alarm task
 // RU: Размер стека для задачи охранно-пожарной сигнализации
-#define CONFIG_ALARM_STACK_SIZE 3072
+#define CONFIG_ALARM_STACK_SIZE 1024*5
 // EN: Queue size for the fire alarm task
 // RU: Размер очереди для задачи охранно-пожарной сигнализации
 #define CONFIG_ALARM_QUEUE_SIZE 32
 // EN: The priority of the fire alarm task
 // RU: Приоритет задачи задачи охранно-пожарной сигнализации
-#define CONFIG_ALARM_PRIORITY CONFIG_DEFAULT_TASK_PRIORITY + 3
+#define CONFIG_ALARM_PRIORITY CONFIG_DEFAULT_TASK_PRIORITY+3
 // EN: Processor core for the fire alarm task
 // RU: Ядро процессора для задачи охранно-пожарной сигнализации
 #define CONFIG_ALARM_CORE 1
+
+// EN: Flash mode on input signal
+// RU: Режим вспышек при входящем сигнале
+#define CONFIG_ALARM_INCOMING_QUANTITY 3
+#define CONFIG_ALARM_INCOMING_DURATION 50
+#define CONFIG_ALARM_INCOMING_INTERVAL 50
