9040d9968009feb3dd4a8716dee6e3a62ced3242
kotyara12 <kotyara12@users.noreply.github.com>
2021-11-29 23:18:11 +0300

    Fixed reconnecting to WiFi in some cases

--- def_wifi.h
+++ def_wifi.h
@@ -7,4 +7,12 @@
 // -----------------------------------------------------------------------------------------------------------------------
 #define CONFIG_WIFI_STATIC_ALLOCATION 1
 #define CONFIG_WIFI_TIMEOUT 30000
-#define CONFIG_WIFI_CONNECT_ATTEMPTS 10
+// EN: The number of attempts to reconnect to the same access point (in case of a sudden disconnection)
+// RU: Количество попыток переподключиться к одной и той же точке доступа (в случае внезапного отключения)
+#define CONFIG_WIFI_RECONNECT_ATTEMPTS 30
+// EN: The number of connection attempts before restarting the WiFi interface (must be greater than CONFIG_WIFI_RECONNECT_ATTEMPTS)
+// RU: Количество попыток подключения до перезапуска WiFi интерфейса (должно быть больше CONFIG_WIFI_RECONNECT_ATTEMPTS)
+#define CONFIG_WIFI_RESTART_ATTEMPTS 100
+// EN: Pause between reconnection attempts to the same access point
+// RU: Пауза между попытками переподключения к одной и той же точке доступа 
+#define CONFIG_WIFI_RECONNECT_DELAY 3000
