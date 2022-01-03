e68dc75d37914d450016cc8425cd25a4054cf506
kotyara12 <kotyara12@users.noreply.github.com>
2022-01-03 09:46:45 +0300

    Slightly changed message templates

--- def_wifi.h
+++ def_wifi.h
@@ -6,7 +6,7 @@
 // ------------------------------------------------ RU - WiFi сети -------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------------
 #define CONFIG_WIFI_STATIC_ALLOCATION 1
-#define CONFIG_WIFI_TIMEOUT 30000
+#define CONFIG_WIFI_TIMEOUT 60000
 // EN: The number of attempts to reconnect to the same access point (in case of a sudden disconnection)
 // RU: Количество попыток переподключиться к одной и той же точке доступа (в случае внезапного отключения)
 #define CONFIG_WIFI_RECONNECT_ATTEMPTS 30
