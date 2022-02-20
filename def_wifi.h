8281f384bc6a119f766ca7bf05a205bfed16fb96
kotyara12 <kotyara12@yandex.ru>
2022-02-20 17:10:29 +0300

    Settings for RSSI level control

--- def_wifi.h
+++ def_wifi.h
@@ -16,3 +16,13 @@
 // EN: Pause between reconnection attempts to the same access point
 // RU: Пауза между попытками переподключения к одной и той же точке доступа 
 #define CONFIG_WIFI_RECONNECT_DELAY 3000
+// EN: Parameters
+// RU: Параметры
+#define CONFIG_WIFI_PGROUP_KEY "wifi"
+#define CONFIG_WIFI_PGROUP_TOPIC "wifi"
+#define CONFIG_WIFI_PGROUP_FRIENDLY "WiFi"
+// EN: RSSI threshold value above which some network operations (for example, sending "big" data) will be suspended
+// RU: Пороговое значение RSSI, при превышении которого некоторые сетевые операции (например отправка "больших" данных) будут приостановлены
+#define CONFIG_WIFI_RSSI_THERSHOLD 90
+#define CONFIG_WIFI_RSSI_THERSHOLD_KEY "rssi_threshold"
+#define CONFIG_WIFI_RSSI_THERSHOLD_FRIENDLY "Пороговое значение RSSI"
