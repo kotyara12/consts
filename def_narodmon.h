7927d718dc96acfe7d7b5347933072f147829064
kotyara12 <kotyara12@yandex.ru>
2023-02-15 10:52:53 +0300

    Adaptation to ESP-IDF 5.0.0

--- def_narodmon.h
+++ def_narodmon.h
@@ -7,15 +7,15 @@
 // -----------------------------------------------------------------------------------------------------------------------
 // EN: API connection options
 // RU: Параметры подключения к API 
-#define CONFIG_NARODMON_TLS_PEM_STORAGE CONFIG_DEFAULT_TLS_STORAGE
-#define CONFIG_NARODMON_TLS_PEM_START   CONFIG_DEFAULT_TLS_PEM_START
-#define CONFIG_NARODMON_TLS_PEM_END     CONFIG_DEFAULT_TLS_PEM_END
-#define CONFIG_NARODMON_API_HOST        "narodmon.ru"
-#define CONFIG_NARODMON_API_PORT        443
-#define CONFIG_NARODMON_API_SEND_PATH   "/get"
-#define CONFIG_NARODMON_API_SEND_VALUES "ID=%s&time=%d&%s"
-#define CONFIG_NARODMON_API_TIMEOUT_MS  30000
-#define CONFIG_NARODMON_API_LOGTAG      "NrMn"
+#define CONFIG_NARODMON_TLS_PEM_STORAGE     CONFIG_DEFAULT_TLS_STORAGE
+#define CONFIG_NARODMON_TLS_PEM_START       CONFIG_DEFAULT_TLS_PEM_START
+#define CONFIG_NARODMON_TLS_PEM_END         CONFIG_DEFAULT_TLS_PEM_END
+#define CONFIG_NARODMON_API_HOST            "narodmon.ru"
+#define CONFIG_NARODMON_API_PORT             443
+#define CONFIG_NARODMON_API_SEND_PATH        "/get"
+#define CONFIG_NARODMON_API_SEND_VALUES      "ID=%s&%s"
+#define CONFIG_NARODMON_API_TIMEOUT_MS       30000
+#define CONFIG_NARODMON_API_LOGTAG           "NrMn"
 
 // EN: Minimum server access interval in milliseconds
 // RU: Минимальный интервал обращения к серверу в миллисекундах
