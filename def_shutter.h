3ace4c5a6e3c52526f5287d70427081da38ff841
kotyara12 <kotyara12@yandex.ru>
2023-05-18 11:17:29 +0300

    Added options for shutter

---
+++ def_shutter.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "project_config.h"
+
+// EN: String identifiers for JSON
+// RU: Строковые идентификаторы для JSON
+#define CONFIG_SHUTTER_OPEN "open"
+#define CONFIG_SHUTTER_CLOSE "close"
+#define CONFIG_SHUTTER_CHANGED "changed"
+#define CONFIG_SHUTTER_STATUS "status"
+#define CONFIG_SHUTTER_VALUE "steps"
+#define CONFIG_SHUTTER_PERCENT "percent"
+#define CONFIG_SHUTTER_MAXIMUM  "maximum"
+#define CONFIG_SHUTTER_TIMESTAMP "timestamp"
+
+// EN: Timestamp format
+// RU: Формат отметок времени
+#define CONFIG_SHUTTER_TIMESTAMP_BUF_SIZE 20
+#define CONFIG_SHUTTER_TIMESTAMP_FORMAT "%d.%m.%Y %H:%M:%S"
+
